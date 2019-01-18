0cad17f6da5cd53a96c0f4fc1d1f87de56e5ffdd
Natpakal K <jam13432@gmail.com>
2019-01-19 03:45:29 +0700

    Update token.js

--- token.js
+++ token.js
@@ -2,9 +2,7 @@ var token;
 
 module.exports =  { 
     sendToken: function () {
-        var token;
-        // token = 'NTAxNzcxNjYyOTU0NDYzMjMy.DrXmcA.vh-0tz8rqLMzs6TVOo80Gl9DpoA'; /* DEV */
-        token = 'Mzk1MTU0MTkyMzI4ODE4Njg5.DTomUA.rhwh1qGyQ9TbAkxvb08OHfNodP8'; /* Production */
+        var token = <YOUR_BOT_TOKEN> ;
         return token;
     }
 }
