efed83371ad2b3b7af414a8208b196fa5a493d4a
NatpakalJAM <jam13432@gmail.com>
2019-04-18 17:06:54 +0700

    [in progress] add search youtube by keyword or url

--- commands/music/music.js
+++ commands/music/music.js
@@ -1,16 +1,29 @@
+const botConfig = require('../../config/config.json');
+const YouTube = require("discord-youtube-api");
+const youtube = new YouTube(botConfig.YouTube_API);
+
+var video;
+
 module.exports = {
-    musicCom: function (message) {
-        var VC = message.member.voiceChannel;
-        if (!VC) {
-            return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL");
+    musicCom: async function (message) {
+        var messageRecsive = message.content.replace('music ', '');
+        if (messageRecsive.match(/^http/)) {
+            video = await youtube.getVideo(messageRecsive);
+        } else {
+            video = await youtube.searchVideos(messageRecsive);
         }
-        VC.join()
-            .then(connection => {
-                const dispatcher = connection.playFile('./commands/music/mp3/audiofile.mp3');
-                dispatcher.on("end", end => {
-                    VC.leave();
-                });
-            })
-            .catch(console.error);
+        console.log(video.url);
+        // var VC = message.member.voiceChannel;
+        // if (!VC) {
+        //     return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL");
+        // }
+        // VC.join()
+        //     .then(connection => {
+        //         const dispatcher = connection.playFile('./commands/music/mp3/audiofile.mp3');
+        //         dispatcher.on("end", end => {
+        //             VC.leave();
+        //         });
+        //     })
+        //     .catch(console.error);
     }
 }
