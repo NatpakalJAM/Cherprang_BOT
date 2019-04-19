2d25ad3735df3601c813e887d44b611b7a28d8ce
NatpakalJAM <jam13432@gmail.com>
2019-04-19 17:05:59 +0700

    youtube streaminggit status

--- commands/music/music.js
+++ commands/music/music.js
@@ -1,29 +1,46 @@
 const botConfig = require('../../config/config.json');
 const YouTube = require("discord-youtube-api");
-const youtube = new YouTube(botConfig.YouTube_API);
+const ytdl = require('ytdl-core');
 
-var video;
+const youtube = new YouTube(botConfig.YouTube_API);
+const streamOptions = {
+    seek: 0,
+    volume: 1
+};
 
 module.exports = {
-    musicCom: async function (message) {
-        var messageRecsive = message.content.replace('music ', '');
-        if (messageRecsive.match(/^http/)) {
-            video = await youtube.getVideo(messageRecsive);
-        } else {
-            video = await youtube.searchVideos(messageRecsive);
+    musicCom: function (message) {
+        var voice_channel = message.member.voiceChannel;
+        if (!voice_channel) {
+            return message.reply("join voice channel ก่อนสิคะ");
         }
-        console.log(video.url);
-        // var VC = message.member.voiceChannel;
-        // if (!VC) {
-        //     return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL");
-        // }
-        // VC.join()
-        //     .then(connection => {
-        //         const dispatcher = connection.playFile('./commands/music/mp3/audiofile.mp3');
-        //         dispatcher.on("end", end => {
-        //             VC.leave();
-        //         });
-        //     })
-        //     .catch(console.error);
+
+        youtubeSearch(message.content.replace('music ', '')).then(function (url) {
+            voice_channel.join().then(connection => {
+                const stream = ytdl(url, {
+                    filter: 'audioonly'
+                });
+                const dispatcher = connection.playStream(stream, streamOptions)
+                    .on("end", () => {
+                        console.log("Music Ended")
+                        voice_channel.leave()
+                    })
+                    .on("error", error => {
+                        console.error(error)
+                    });
+            });
+        });
     }
+}
+
+async function youtubeSearch(messageRecsive) {
+    var video;
+    // var messageRecsive = message.content.replace('music ', '');
+    if (messageRecsive.match(/^http/)) {
+        video = await youtube.getVideo(messageRecsive);
+    } else {
+        video = await youtube.searchVideos(messageRecsive);
+    }
+    var returnURL = video.url;
+    return returnURL;
 }
